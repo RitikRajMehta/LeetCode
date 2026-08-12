bdee104e982e068a5aa286af7b562faabb255a38
Ritik Raj Mehta <108204970+RitikRajMehta@users.noreply.github.com>
2026-08-12 23:47:47 +0530

    Time: 26 ms (100%), Space: 109.7 MB (9.2%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,22 +1,29 @@
-import java.util.*;
-
 class Solution {
+    static class Counter {
+        int cnt = 0;
+    }
     public int maxSubarrayLength(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        int left = 0, ans = 0;
+        int N = nums.length;
+        Map<Integer, Counter> map = new HashMap<>();
 
-        for (int right = 0; right < nums.length; right++) {
-            int x = nums[right];
-            freq.put(x, freq.getOrDefault(x, 0) + 1);
-
-            while (freq.get(x) > k) {
-                int y = nums[left++];
-                freq.put(y, freq.get(y) - 1);
+        int ll = 0;
+        int rr = 0;
+        int result = 0;
+        while (rr < N) {
+            int num = nums[rr++];
+            Counter counter = map.get(num);
+            if ( counter == null ) map.put(num, counter = new Counter());
+            if ( counter.cnt < k ) {
+                counter.cnt++;
+            } else {
+                int num2 = 0;
+                while ( (num2 = nums[ll++]) != num ) {
+                    Counter counter2 = map.get(num2);
+                    counter2.cnt--;
+                }
             }
-
-            ans = Math.max(ans, right - left + 1);
+            result = Math.max(result, rr - ll);
         }
-
-        return ans;
+        return result;
     }
 }
